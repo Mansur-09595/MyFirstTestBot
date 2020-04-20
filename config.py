34cb8e19b1dd10882af13d03f5c503a30a6f7870
Mansur-09595 <mansurmusaev.work@mail.ru>
2020-04-20 21:56:25 +0300

    fix gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = '956317453:AAHD5x5ietMr4uR235VjXp6aXvpJzmIaQaE' #API - KEY от телеграмма
-
-#Proxy сервера для обхода блокировки телеграмма
-PROXY = {'proxy_url': 'socks5://96.96.33.133:1080',
-        'urllib3_proxy_kwargs': {'username': 'some',
-        'password': 'one'}}
